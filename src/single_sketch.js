58af6110e92e00d2606f3042f4cbc3c2be2fa3f1
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2023-05-20 22:56:41 -0600

    initialize using p5 temp
    underlying display and copy paste example added and working

---
+++ src/single_sketch.js
@@ -0,0 +1,19 @@
+import { sketch } from 'p5js-wrapper';
+
+sketch.setup = function () {
+  createCanvas(800, 600);
+}
+
+sketch.draw = function () {
+  background(100);
+  fill(255, 0, 0);
+  noStroke();
+  rectMode(CENTER);
+  rect(mouseX, mouseY, 50, 50);
+}
+
+sketch.mousePressed = function () {
+  console.log('here');
+}
+
+
