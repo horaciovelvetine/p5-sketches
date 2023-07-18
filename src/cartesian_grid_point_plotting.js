085c1bb115de44de0416eb533d2d3ba99d4c2c02
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2023-07-18 13:16:03 -0600

    grid plotter tested reworking after changes to relevant utils

--- src/cartesian_grid_point_plotting.js
+++ src/cartesian_grid_point_plotting.js
@@ -7,15 +7,21 @@ export const X = 0; // index values for relevant coordinate
 export const Y = 1; // axis in the array 
 export const Z = 2;
 
-sketch.setup = function () {
+function calculateRadiusOffsetCoordinates(coordinates) {
+  return [
+    coordinates[X] * 200,
+    coordinates[Y] * 200,
+    coordinates[Z] * 200
+  ]
+}
+
+sketch.setup = () => {
   createCanvas((windowWidth - 650), (windowHeight - 300), WEBGL);
 }
 
-sketch.draw = function () {
+sketch.draw = () => {
   background(51);
   orbitControl();
-  
-  let grid = new CartesianTools(width);
 
   let coordinates = testObjects["teapot"];
   // marks the center of the universe
@@ -23,7 +29,7 @@ sketch.draw = function () {
 
   for (const nodeCord of coordinates) {
     // draws spheres at each node
-    let [x1, y1, z1] = grid.calculateRadiusOffsetCoordinates(nodeCord);
+    let [x1, y1, z1] = calculateRadiusOffsetCoordinates(nodeCord);
     push();
     translate(x1, y1, z1);
     if (nodeCord[X] === 0 && nodeCord[Y] === 0 && nodeCord[Z] === 0) {
@@ -34,19 +40,19 @@ sketch.draw = function () {
   }
 
   for (const nextNodeIter of coordinates) {
-    let [x1, y1, z1] = grid.calculateRadiusOffsetCoordinates(nextNodeIter);
+    let [x1, y1, z1] = calculateRadiusOffsetCoordinates(nextNodeIter);
     // draws lines between nodes of a certain distance from each other
     coordinates.forEach((nodeCord) => {
       if (nextNodeIter !== nodeCord) {
-        let [x2, y2, z2] = grid.calculateRadiusOffsetCoordinates(nodeCord);
+        let [x2, y2, z2] = calculateRadiusOffsetCoordinates(nodeCord);
         let distance = dist(x1, y1, z1, x2, y2, z2);
-        if (distance < 700) {
-          stroke(255);
+        if (distance < 125) {
+          strokeWeight(1);
           line(x1, y1, z1, x2, y2, z2);
         }
       }
     });
   }
 
-
 }
+
