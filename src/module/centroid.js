ff2bd296f98c832d9befcd18b95ae26bd8485ca7
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2023-07-29 23:09:07 -0600

    adds implementations of finding centroids working weighted avg
    also adds simple k-means depth 1 search functioning
    
    provides initial vis tools for comparing means of finding avg center point

--- src/module/centroid.js
+++ src/module/centroid.js
@@ -1,6 +1,18 @@
+const X = 0;
+const Y = 1;
+const W = 2;
+
+import kmeans from './kmeans.js';
+
 export class Centroid {
-  constructor() {
+
+
+  constructor(DATASET) {
     // TBD
+    this.DATASET = DATASET;
+    this.weightedAverage = this.findUsingWeightedAverage(DATASET);
+    this.kMeans = this.findUsingKmeans(DATASET);
+    this.kAlsoMeans = this.findUsingKmeans(DATASET);
   }
 
 
@@ -10,33 +22,95 @@ export class Centroid {
     // where x,y represent the coordinates of the point
     // and w represents the weight of the point in relation to the centroid
     let x_cords = pointsArray.map(datPoint => {
-      return datPoint.cords[0];
+      return datPoint.cords[X];
     })
 
     let y_cords = pointsArray.map(datPoint => {
-      return datPoint.cords[1];
+      return datPoint.cords[Y];
     })
 
     let weights = pointsArray.map(datPoint => {
-      return (1/datPoint.cords[2]);
+      return (1 / datPoint.cords[W]);
     })
 
+      ;
     let x = this.weightedAverage(x_cords, weights);
     let y = this.weightedAverage(y_cords, weights);
+
+    let actualDistances = pointsArray.map(datPoint => {
+      return {
+        name: datPoint.name,
+        distance: this.getDistanceBetweenPoints(datPoint, { cords: [x, y] })
+      }
+    });
+
+    let delta = (pointsArray.reduce((acc, datPoint) => {
+      let actual = actualDistances.filter((dat) => dat.name === datPoint.name)[0].distance;
+      let intended = datPoint.cords[W];
+      return acc + Math.abs(actual - intended);
+    }, 0) / pointsArray.length).toFixed(2);
+
+
+
     return {
       name: 'WeightedAverage',
-      cords: [x, y],
-      color: 'white'
+      cords: [x, y, delta],
+      color: 'white',
+      actualDistances: actualDistances,
     };
   }
-  
+
   weightedAverage(values, weights) {
+    // values and weights are arrays of the same length (x || y, w)
     let sum = 0;
     let weightSum = 0;
     for (let i = 0; i < values.length; i++) {
       sum += values[i] * weights[i];
       weightSum += weights[i];
     }
-    return sum / weightSum;
+    return (sum / weightSum).toFixed(2);
   }
+
+  getDistanceBetweenPoints(pointA, pointB) {
+    // get distance between two points on cartesian plane
+    let x1 = pointA.cords[X];
+    let y1 = pointA.cords[Y];
+    let x2 = pointB.cords[X];
+    let y2 = pointB.cords[Y];
+    return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)).toFixed(2);
+
+  }
+
+  findUsingKmeans(DATASET) {
+    // this kMeans will always be used to find the centroid of a kDepth 1 dataset. 
+    let kDepth = 2;
+    let results = kmeans(DATASET, kDepth);
+    console.log(results);
+
+    let actualDistances = DATASET.map(dat => {
+      return {
+        name: dat.name,
+        distance: this.getDistanceBetweenPoints(dat, { cords: [results.centroids[0][X], results.centroids[0][Y]] })
+      }
+    })
+
+    let delta = (DATASET.reduce((acc, datPoint) => {
+      let actual = actualDistances.filter((dat) => dat.name === datPoint.name)[0].distance;
+      let intended = datPoint.cords[W];
+      return acc + Math.abs(actual - intended);
+    }, 0) / DATASET.length).toFixed(2);
+
+    let x = results.centroids[0][X].toFixed(2);
+    let y = results.centroids[0][Y].toFixed(2);
+
+    return {
+      name: 'Kmeans',
+      cords: [x, y, delta],
+      color: 'cyan',
+      actualDistances: actualDistances,
+
+    }
+  }
+
+
 }
