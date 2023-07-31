9d09667256b937c7e768e4d6d13485869137d596
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2023-07-31 01:14:37 -0600

    models now handle data retention and finer display details

--- src/models/centroid.js
+++ src/models/centroid.js
@@ -1,105 +1,55 @@
 import kmeans from '../module/kmeans.js';
-
+import { scaleNumbers } from "../util/scale_numbers";
 export class Centroid {
-  constructor(relatedNodes) {
-    
+  constructor(weightedAvgNode, kMeansNode, relatedNodes) {
+
+    this.weightedAvgNode = weightedAvgNode;
+    this.kMeansNode = kMeansNode;
     this.relatedNodes = relatedNodes;
-    this.weightedAverage = this.findUsingWeightedAverage(relatedNodes);
-    // this.kMeans = this.findUsingKmeans(DATASET);
+    this.name = 'centroid';
+    this.color = 'green';
+    this.x = null;
+    this.y = null;
+    this.z = null;
+    this.s = null;
   }
 
 
-  findUsingWeightedAverage(relatedNodes) {
-    let x_cords = pointsArray.map(datPoint => {
-      return datPoint.cords[X];
+  drawPotentialCentroidVisuals() {
+
+    this.relatedNodes.forEach(node => {
+      node.draw();
+      this.connectCentroidsToRelatedNode(node);
     })
+    this.weightedAvgNode.draw();
+    this.kMeansNode.draw();
 
-    let y_cords = pointsArray.map(datPoint => {
-      return datPoint.cords[Y];
-    })
-
-    let weights = pointsArray.map(datPoint => {
-      return (1 / datPoint.cords[W]);
-    })
-
-      ;
-    let x = this.weightedAverage(x_cords, weights);
-    let y = this.weightedAverage(y_cords, weights);
-
-    let actualDistances = pointsArray.map(datPoint => {
-      return {
-        name: datPoint.name,
-        distance: this.getDistanceBetweenPoints(datPoint, { cords: [x, y] })
-      }
-    });
-
-    let delta = (pointsArray.reduce((acc, datPoint) => {
-      let actual = actualDistances.filter((dat) => dat.name === datPoint.name)[0].distance;
-      let intended = datPoint.cords[W];
-      return acc + Math.abs(actual - intended);
-    }, 0) / pointsArray.length).toFixed(2);
-
-
-
-    return {
-      name: 'WeightedAverage',
-      cords: [x, y, delta],
-      color: 'white',
-      actualDistances: actualDistances,
-    };
+    this.drawPotentialInfoLabels(this.weightedAvgNode, 0);
+    this.drawPotentialInfoLabels(this.kMeansNode, 20);
   }
 
-  weightedAverage(values, weights) {
-    // values and weights are arrays of the same length (x || y, w)
-    let sum = 0;
-    let weightSum = 0;
-    for (let i = 0; i < values.length; i++) {
-      sum += values[i] * weights[i];
-      weightSum += weights[i];
-    }
-    return (sum / weightSum).toFixed(2);
+  drawPotentialInfoLabels(potentialCentroidNode, yOffset) {
+    let [x, y, z, s] = potentialCentroidNode.getCoordinates();
+    fill(potentialCentroidNode.color);
+    stroke(potentialCentroidNode.color);
+    strokeWeight(0.1);
+    textSize(12);
+    text(potentialCentroidNode.labelText(x.toFixed(2), y.toFixed(2), s.toFixed(7)), 145, 370 + yOffset);
   }
 
-  getDistanceBetweenPoints(pointA, pointB) {
-    // get distance between two points on cartesian plane
-    let x1 = pointA.cords[X];
-    let y1 = pointA.cords[Y];
-    let x2 = pointB.cords[X];
-    let y2 = pointB.cords[Y];
-    return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)).toFixed(2);
+  connectCentroidsToRelatedNode(node) {
+    let [x1, y1, z1] = scaleNumbers(...node.getCoordinates());
+    let [x2, y2, z2] = scaleNumbers(...this.weightedAvgNode.getCoordinates());
+    let [x3, y3, z3] = scaleNumbers(...this.kMeansNode.getCoordinates());
 
+    strokeWeight(0.2);
+    stroke(this.weightedAvgNode.color);
+    line(x1, y1, x2, y2);
+    stroke(this.kMeansNode.color);
+    line(x1, y1, x3, y3);
   }
 
-  findUsingKmeans(DATASET) {
-    // this kMeans will always be used to find the centroid of a kDepth 1 dataset. 
-    let kDepth = 2;
-    let results = kmeans(DATASET, kDepth);
-    console.log(results);
 
-    let actualDistances = DATASET.map(dat => {
-      return {
-        name: dat.name,
-        distance: this.getDistanceBetweenPoints(dat, { cords: [results.centroids[0][X], results.centroids[0][Y]] })
-      }
-    })
-
-    let delta = (DATASET.reduce((acc, datPoint) => {
-      let actual = actualDistances.filter((dat) => dat.name === datPoint.name)[0].distance;
-      let intended = datPoint.cords[W];
-      return acc + Math.abs(actual - intended);
-    }, 0) / DATASET.length).toFixed(2);
-
-    let x = results.centroids[0][X].toFixed(2);
-    let y = results.centroids[0][Y].toFixed(2);
-
-    return {
-      name: 'Kmeans',
-      cords: [x, y, delta],
-      color: 'cyan',
-      actualDistances: actualDistances,
-
-    }
-  }
 
 
 }
--- src/models/node.js
+++ src/models/node.js
@@ -1,5 +1,5 @@
 import { scaleNumbers } from "../util/scale_numbers";
-import { X, Y, Z } from "../module/constants";
+import { X, Y, Z, S } from "../module/constants";
 
 export class Node {
   constructor(name, cords, color) {
@@ -7,6 +7,7 @@ export class Node {
     this.x = cords[X];
     this.y = cords[Y];
     this.z = cords[Z];
+    this.s = cords[S]; // temp value represents relationship strength between two nodes, used in calculating a position for related nodes
     this.color = color;
   }
 
@@ -16,16 +17,22 @@ export class Node {
     this.z = z;
   }
 
+  getCoordinates() {
+    return [Number(this.x), Number(this.y), Number(this.z), Number(this.s)];
+  }
+
   draw() {
     strokeWeight(10);
     stroke(this.color);
+    fill(this.color);
     let [x, y, z] = scaleNumbers(this.x, this.y, this.z);
     point(x, y, z);
+    strokeWeight(0.1);
   }
 
-  drawLabel() { }
 
-  labelText() {
-    return this.name + ': (' + this.x + ', ' + this.y + ', ' + this.z + ')'
+
+  labelText(x, y, z) {
+    return this.name + ': (' + x + ', ' + y + ', ' + z + ')'
   }
 }
