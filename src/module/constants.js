18311d5bfa8bdf203d677257f756768a1f4d06ae
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2023-07-31 01:14:51 -0600

    add some baseline configs to prevent more magic numbers

--- src/module/constants.js
+++ src/module/constants.js
@@ -1,4 +1,8 @@
+//* Base constants and config values for calculations
+
 export const X = 0;
 export const Y = 1;
 export const Z = 2;
 export const S = 3; //==> strength
+export const K = 1; //==> kmeans value for depth
+export const SCALE_MULTIPLIER = 350;
